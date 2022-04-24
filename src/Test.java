3f662e9f30ecf6568da417df31be248b8f230f22
shalabhraj1990 <shalabhrajetc@gmail.com>
2022-04-24 14:00:19 +0530

    minor change

--- src/Test.java
+++ src/Test.java
@@ -1,54 +1,64 @@
-import com.ds.stack.StackDemo;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 class Test {
 	public static void main(String[] args) {
-		StackDemo st = new StackDemo();
-		
-		st.push(4);
-		st.push(7);
-		st.push(2);
-		st.push(9);
-		//st.pop();
-		System.out.println(st.top());
-		
-		
-//		int[] arr = new int[] { 3, 6, 7, 11 };
-//		System.out.println(minEatingSpeed(arr, 8));
+
+		int[][] nums = { { 44,21,48 } };
+		int s = nums.length;
+		if(s <= 1) {
+			 convetToList(nums[0]).stream().forEach(System.out::println);
+		}
+		if (s == 2) {
+			int[] ans = intersectionOfAray(nums[0], nums[1]);
+			 convetToList(ans).stream().forEach(System.out::println);
+
+		} if else {
+			int startIndex = 2;
+			int[] ans = intersectionOfAray(nums[0], nums[1]);
+			while (startIndex < s) {
+				int[] nextArry = nums[startIndex];
+				ans = intersectionOfAray(ans, nextArry);
+				startIndex++;
+			}
+
+			convetToList(ans).stream().forEach(System.out::println);
+
+		}
+
 	}
 
-	public static int minEatingSpeed(int[] piles, int h) {
-		int start = 1;
-		//int ans = -1;
-		int max = piles[0];
-		for (int i = 0; i < piles.length; i++) {
-			if (piles[i] >= max) {
-				max = piles[i];
+	public static List<Integer> convetToList(int[] ans) {
+		List<Integer> list = new ArrayList<Integer>();
+		for (int i = 0; i < ans.length; i++) {
+			if (ans[i] != 0)
+				list.add(ans[i]);
+		}
+		
+		Collections.sort(list); 
+		return list;
+	}
+
+	public static int[] intersectionOfAray(int[] arr1, int[] arr2) {
+		int n = arr1.length;
+		int m = arr2.length;
+		int i, j;
+		int[] list = new int[1000];
+		int k = 0;
+
+		for (i = 0; i < n; i++) {
+			int num = arr1[i];
+			for (j = 0; j < m; j++) {
+				if (num == arr2[j]) {
+					list[k] = num;
+					k++;
+				}
 			}
 		}
 
-		while (start <= max) {
-			int noOfHrs = 0;
-			int mid = (int) ((start + max) / 2);
-			for (int pile : piles) {
-				int t = (int) Math.ceil((double) pile /  mid);
-				noOfHrs += (int) t;
-			}
-
-			if ((int) noOfHrs > h) {
-				start = mid + 1;
-			}
-
-			else if ((int) noOfHrs < h) {
-
-				max = mid - 1;
-			} else {
-			//	ans = mid;
-				max = mid - 1;
-			}
-		}
-
-		return  start;
-
+		return list;
 	}
 
 }
